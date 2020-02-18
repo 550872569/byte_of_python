b0078bd3603cce5fee4b91920f0646bdbd41f89a
YY <550872569@qq.com>
2020-02-18 17:18:26 +0800

    learn Python Set

---
+++ python/Set/Set.py
@@ -0,0 +1,255 @@
+'''
+        set() -> new empty set object
+        set(iterable) -> new set object
+
+        Build an unordered collection of unique elements.
+        # (copied from class doc)
+
+'''
+
+'''
+class set(object):
+    """
+    set() -> new empty set object
+    set(iterable) -> new set object
+    
+    Build an unordered collection of unique elements.
+    """
+    def add(self, *args, **kwargs): # real signature unknown
+        """
+        Add an element to a set.
+        
+        This has no effect if the element is already present.
+        """
+        pass
+
+    def clear(self, *args, **kwargs): # real signature unknown
+        """ Remove all elements from this set. """
+        pass
+
+    def copy(self, *args, **kwargs): # real signature unknown
+        """ Return a shallow copy of a set. """
+        pass
+
+    def difference(self, *args, **kwargs): # real signature unknown
+        """
+        Return the difference of two or more sets as a new set.
+        
+        (i.e. all elements that are in this set but not the others.)
+        """
+        pass
+
+    def difference_update(self, *args, **kwargs): # real signature unknown
+        """ Remove all elements of another set from this set. """
+        pass
+
+    def discard(self, *args, **kwargs): # real signature unknown
+        """
+        Remove an element from a set if it is a member.
+        
+        If the element is not a member, do nothing.
+        """
+        pass
+
+    def intersection(self, *args, **kwargs): # real signature unknown
+        """
+        Return the intersection of two sets as a new set.
+        
+        (i.e. all elements that are in both sets.)
+        """
+        pass
+
+    def intersection_update(self, *args, **kwargs): # real signature unknown
+        """ Update a set with the intersection of itself and another. """
+        pass
+
+    def isdisjoint(self, *args, **kwargs): # real signature unknown
+        """ Return True if two sets have a null intersection. """
+        pass
+
+    def issubset(self, *args, **kwargs): # real signature unknown
+        """ Report whether another set contains this set. """
+        pass
+
+    def issuperset(self, *args, **kwargs): # real signature unknown
+        """ Report whether this set contains another set. """
+        pass
+
+    def pop(self, *args, **kwargs): # real signature unknown
+        """
+        Remove and return an arbitrary set element.
+        Raises KeyError if the set is empty.
+        """
+        pass
+
+    def remove(self, *args, **kwargs): # real signature unknown
+        """
+        Remove an element from a set; it must be a member.
+        
+        If the element is not a member, raise a KeyError.
+        """
+        pass
+
+    def symmetric_difference(self, *args, **kwargs): # real signature unknown
+        """
+        Return the symmetric difference of two sets as a new set.
+        
+        (i.e. all elements that are in exactly one of the sets.)
+        """
+        pass
+
+    def symmetric_difference_update(self, *args, **kwargs): # real signature unknown
+        """ Update a set with the symmetric difference of itself and another. """
+        pass
+
+    def union(self, *args, **kwargs): # real signature unknown
+        """
+        Return the union of sets as a new set.
+        
+        (i.e. all elements that are in either set.)
+        """
+        pass
+
+    def update(self, *args, **kwargs): # real signature unknown
+        """ Update a set with the union of itself and others. """
+        pass
+
+    def __and__(self, *args, **kwargs): # real signature unknown
+        """ Return self&value. """
+        pass
+
+    def __contains__(self, y): # real signature unknown; restored from __doc__
+        """ x.__contains__(y) <==> y in x. """
+        pass
+
+    def __eq__(self, *args, **kwargs): # real signature unknown
+        """ Return self==value. """
+        pass
+
+    def __getattribute__(self, *args, **kwargs): # real signature unknown
+        """ Return getattr(self, name). """
+        pass
+
+    def __ge__(self, *args, **kwargs): # real signature unknown
+        """ Return self>=value. """
+        pass
+
+    def __gt__(self, *args, **kwargs): # real signature unknown
+        """ Return self>value. """
+        pass
+
+    def __iand__(self, *args, **kwargs): # real signature unknown
+        """ Return self&=value. """
+        pass
+
+    def __init__(self, seq=()): # known special case of set.__init__
+        """
+        set() -> new empty set object
+        set(iterable) -> new set object
+        
+        Build an unordered collection of unique elements.
+        # (copied from class doc)
+        """
+        pass
+
+    def __ior__(self, *args, **kwargs): # real signature unknown
+        """ Return self|=value. """
+        pass
+
+    def __isub__(self, *args, **kwargs): # real signature unknown
+        """ Return self-=value. """
+        pass
+
+    def __iter__(self, *args, **kwargs): # real signature unknown
+        """ Implement iter(self). """
+        pass
+
+    def __ixor__(self, *args, **kwargs): # real signature unknown
+        """ Return self^=value. """
+        pass
+
+    def __len__(self, *args, **kwargs): # real signature unknown
+        """ Return len(self). """
+        pass
+
+    def __le__(self, *args, **kwargs): # real signature unknown
+        """ Return self<=value. """
+        pass
+
+    def __lt__(self, *args, **kwargs): # real signature unknown
+        """ Return self<value. """
+        pass
+
+    @staticmethod # known case of __new__
+    def __new__(*args, **kwargs): # real signature unknown
+        """ Create and return a new object.  See help(type) for accurate signature. """
+        pass
+
+    def __ne__(self, *args, **kwargs): # real signature unknown
+        """ Return self!=value. """
+        pass
+
+    def __or__(self, *args, **kwargs): # real signature unknown
+        """ Return self|value. """
+        pass
+
+    def __rand__(self, *args, **kwargs): # real signature unknown
+        """ Return value&self. """
+        pass
+
+    def __reduce__(self, *args, **kwargs): # real signature unknown
+        """ Return state information for pickling. """
+        pass
+
+    def __repr__(self, *args, **kwargs): # real signature unknown
+        """ Return repr(self). """
+        pass
+
+    def __ror__(self, *args, **kwargs): # real signature unknown
+        """ Return value|self. """
+        pass
+
+    def __rsub__(self, *args, **kwargs): # real signature unknown
+        """ Return value-self. """
+        pass
+
+    def __rxor__(self, *args, **kwargs): # real signature unknown
+        """ Return value^self. """
+        pass
+
+    def __sizeof__(self): # real signature unknown; restored from __doc__
+        """ S.__sizeof__() -> size of S in memory, in bytes """
+        pass
+
+    def __sub__(self, *args, **kwargs): # real signature unknown
+        """ Return self-value. """
+        pass
+
+    def __xor__(self, *args, **kwargs): # real signature unknown
+        """ Return self^value. """
+        pass
+
+    __hash__ = None
+
+
+'''
+setDays = set()
+print(setDays)
+# set()
+print(type(setDays))
+# <class 'set'>
+setDays.add('Monday')
+print(setDays)
+# {'Monday'}
+
+setCopyDays = setDays.copy()
+print(setCopyDays)
+setCopyDays.add('Friday')
+print(setDays)
+# {'Monday'}
+print(setCopyDays)
+# {'Friday', 'Monday'}
+print(setDays.difference(setCopyDays))
+# set()
+print(setCopyDays.difference(setDays))
+# {'Friday'}
